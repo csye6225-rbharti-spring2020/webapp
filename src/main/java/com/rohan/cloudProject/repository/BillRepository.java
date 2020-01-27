c6ea1a30934537490a8a99fd4f40c27f6f344441
Rohan Bharti <bharti.r@northeastern.edu>
2020-01-27 02:18:29 -0500

    Added two new methods to the Bill repo

--- src/main/java/com/rohan/cloudProject/repository/BillRepository.java
+++ src/main/java/com/rohan/cloudProject/repository/BillRepository.java
@@ -4,6 +4,9 @@ import com.rohan.cloudProject.model.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Bill Repository interface for the Spring Boot Application.
  *
@@ -11,4 +14,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BillRepository extends JpaRepository<Bill, String> {
+
+    List<Bill> findByUserId(String userId);
+
+    Optional<Bill> findByIdAndUserId(String billId, String userId);
+
 }
