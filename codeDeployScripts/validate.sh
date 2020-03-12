699722c70ae4a4e2bc24ffe1339d765557331bc9
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-12 08:14:54 -0400

    Updated the validate scirpt

--- codeDeployScripts/validate.sh
+++ codeDeployScripts/validate.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-echo "Waiting for 20 seconds to check the health of the application"
-sleep 20
-
-res=`curl -s --head http://localhost:8080/actuator/health | head -n 1 | grep -c HTTP/1.1 200 OK`
-
-if [ $res -eq 1 ]
-then
-MSG = " OKAY"
-exit 0
-else
-MSG = " NOT OKAY"
-exit 1
-fi
+while [ true ]
+do
+    if [ "$(curl -s http://localhost:8080/actuator/health)" = '{"status":"UP"}' ]
+    then
+        exit 0
+    else
+        echo "check server is running?"
+        sleep 3s
+    fi
+done
