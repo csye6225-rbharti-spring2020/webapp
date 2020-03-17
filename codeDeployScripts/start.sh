032fef5214633badafbbc0d056e4a5ba505385dc
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-16 23:39:34 -0400

    Modified the appspec

--- codeDeployScripts/start.sh
+++ codeDeployScripts/start.sh
@@ -4,7 +4,6 @@ cd /home/ubuntu/myApp/
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/myApp/*
 sudo chmod +x ROOT.jar
-java -jar ROOT.jar
 
 source /etc/profile.d/envvariable.sh
 nohup java -jar ROOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
