8eb4cb820802873adee1dd10c979e2bac61b72b4
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-16 20:12:50 -0400

    Running as ubuntu

--- codeDeployScripts/start.sh
+++ codeDeployScripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu/myApp/
 source /etc/profile.d/envvariable.sh
-sudo java -jar ROOT.jar > /dev/null 2> /dev/null < /dev/null &
+cd /home/ubuntu/myApp/
+java -jar ROOT.jar
