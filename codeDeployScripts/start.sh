a8e262f8cea2f46f5c55c2e9b6b537935891849a
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-17 03:12:14 -0400

    Modified the config

--- codeDeployScripts/start.sh
+++ codeDeployScripts/start.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/myApp/
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/myApp/*
-sudo chmod +x ROOT.jar
+sudo chmod +x cloudproject-0.0.1-SNAPSHOT.jar
 
 source /etc/profile.d/envvariable.sh
-nohup java -jar ROOT.jar > /home/ubuntu/applog.txt 2> /home/ubuntu/applog.txt < /home/ubuntu/applog.txt &
+nohup java -jar cloudproject-0.0.1-SNAPSHOT.jar > /home/ubuntu/applog.txt 2> /home/ubuntu/applog.txt < /home/ubuntu/applog.txt &
