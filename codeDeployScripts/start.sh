926d452bc962dc81ecc9996d6a55a22bf81fe1fa
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-17 00:18:44 -0400

    Modified the appspec

--- codeDeployScripts/start.sh
+++ codeDeployScripts/start.sh
@@ -6,4 +6,4 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/myApp/*
 sudo chmod +x ROOT.jar
 
 source /etc/profile.d/envvariable.sh
-nohup java -jar ROOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
+nohup java -jar -Dspring.profiles.active=${appProfile} ROOT.jar > /home/ubuntu/myApp/log.txt 2> /home/ubuntu/myApp/log.txt < /home/ubuntu/myApp/log.txt &
