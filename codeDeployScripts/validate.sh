7ab81104c87898b449a2db31bb67be902863e442
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-24 17:41:59 -0400

    Modified the validate script

--- codeDeployScripts/validate.sh
+++ codeDeployScripts/validate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-response_code=$(sudo curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/actuator/health)
-
-if [[ "$response_code" -ne 200 ]] ; then
-  echo "The application is responding with $response_code"
-  exit 1
-else
-  echo "Actuator Health Endpoint Worked Successfully - $response_code"
-  exit 0
-fi
+for i in `seq 1 10`;
+do
+  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080/actuator/health`
+  if [ "$HTTP_CODE" == "200" ]; then
+    echo "Actuator Health Endpoint Worked Successfully - $response_code"
+    exit 0;
+  fi
+  echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
+  sleep 10
+done
+echo "Server did not come up after expected time. Failing."
+exit 1
