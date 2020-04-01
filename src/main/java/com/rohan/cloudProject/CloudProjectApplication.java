0ec1f44d031044c1b3ebde45c9146c173e2a3ba9
Rohan Bharti <bharti.r@northeastern.edu>
2020-04-01 15:52:27 -0400

    Enabled Scheduling

--- src/main/java/com/rohan/cloudProject/CloudProjectApplication.java
+++ src/main/java/com/rohan/cloudProject/CloudProjectApplication.java
@@ -3,11 +3,13 @@ package com.rohan.cloudProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableJpaRepositories("com.rohan.cloudProject.repository")
+@EnableScheduling
 public class CloudProjectApplication {
 
     public static void main(String[] args) {
